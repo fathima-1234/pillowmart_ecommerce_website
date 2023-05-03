bb233764d5f9f28bdc231b3a61e5aaed529d9445
fathima-1234 <fathimasuhra19@gmail.com>
2023-05-03 16:16:06 +0530

    login updated

--- pillowmart/settings.py
+++ pillowmart/settings.py
@@ -156,6 +156,6 @@ OTP_SECRET=''
 
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
-EMAIL_HOST_USER='fathimasuhra19@gmail.com'
-EMAIL_HOST_PASSWORD='mouyyylwmhxolcdb'
+EMAIL_HOST_USER='ahammadnooh@gmail.com'
+EMAIL_HOST_PASSWORD='qximrzbkwiltwkrc'
 EMAIL_USE_TLS=True
